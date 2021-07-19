d9548d0d3f134285473a2c32fe16215e202a6b97
ghost547 <87426847+ghost547@users.noreply.github.com>
2021-07-19 16:45:42 +0800

    Update thumbnail.js

--- thumbnail.js
+++ thumbnail.js
@@ -1,8 +1,12 @@
 document.addEventListener("DOMContentLoaded", function(event) {
 	
- let targetImage = document.querySelector("#smart-image");
- targetImage.addEventListener("click", function() {
-    targetImage.classList.remove("small");
- });
-
-});
+    let targetImage = document.querySelector("#smart-image2");
+    targetImage.addEventListener("click", function() {
+        if (targetImage.classList.contains("small")) {
+            targetImage.classList.remove("small");
+        }else{
+            targetImage.classList.add("small");        
+        }
+    });
+	
+	    });
